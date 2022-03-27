8135919e7eb5128705d8969ad466ee04e9063f47
waki285 <suzuneu@suzuneu.com>
2022-03-27 11:32:07 +0900

    add cooldown

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const ms = require("ms");
+
+module.exports = {
+  normalUserCooldown: ms("6h"),
+  premiumUserCooldown: ms("1h"),
+};
--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const client = new Client({
 const fs = require("fs");
 
 const prefix = ".";
+/** @type {WeakMap<string, number>} */
+const cooldowns = new Map();
+const config = require("../config/config.js");
 
 client.on("ready", () => {
   console.log("Ready!");
@@ -45,14 +48,15 @@ client.on("messageCreate", async (message) => {
     message.reply({ embeds: [embed] });
   } else if (command === "stock") {
     const files = fs.readdirSync("./files").filter((file) => file.endsWith(".txt"));
-    const zaikos = files.map((file) => fs.readFileSync(`./files/${file}`, "utf8").length);
+    const zaikos = files.map((file) => fs.readFileSync(`./files/${file}`, "utf8").split("\n").length);
     const embed = new MessageEmbed()
       .setTitle("在庫")
-      .setDescription(`${files.map((x, i) => `${x}: ${zaikos[i]}`).join("\n")}`)
+      .setDescription(`${files.map((x, i) => `${x.replace(/\.txt$/, "")}: ${zaikos[i]}`).join("\n")}`)
       .setColor("RANDOM")
       .setTimestamp();
     message.reply({ embeds: [embed] });
   } else if (command === "gen") {
+    
     const files = fs.readdirSync("./files").filter((file) => file.endsWith(".txt"));
     const hosiimo = args.join(" ").toLowerCase();
     const file = files.find(x => x === hosiimo + ".txt");
@@ -67,6 +71,42 @@ client.on("messageCreate", async (message) => {
     }
     const nakami = fs.readFileSync(`./files/${file}`, "utf8").split("\n")
     const ejected = nakami[0];
+    if (!ejected) {
+      const embed = new MessageEmbed()
+        .setTitle("エラー")
+        .setDescription(`${hosiimo} の在庫はありません`)
+        .setColor("RANDOM")
+        .setTimestamp();
+      message.reply({ embeds: [embed] });
+      return;
+    }
+    if (cooldowns.has(message.author.id)) {
+      const time = cooldowns.get(message.author.id);
+      const now = Date.now();
+      const premiumUsers = fs.readFileSync("./config/premium.txt", "utf8").split("\n");
+      if (premiumUsers.includes(message.author.id)) {
+        if ((now - time) < config.premiumUserCooldown) {
+          const embed = new MessageEmbed()
+            .setTitle("エラー")
+            .setDescription(`あなたはまだクールダウン中です。\n${(config.premiumUserCooldown - (now - time)) / 1000}秒後に再度実行してください。`)
+            .setColor("RANDOM")
+            .setTimestamp();
+          message.reply({ embeds: [embed] });
+          return;
+        }
+      } else {
+        if ((now - time) < config.normalUserCooldown) {
+          const embed = new MessageEmbed()
+            .setTitle("エラー")
+            .setDescription(`あなたはまだクールダウン中です。\n${(config.normalUserCooldown - (now - time)) / 1000}秒後に再度実行してください。`)
+            .setColor("RANDOM")
+            .setTimestamp();
+          message.reply({ embeds: [embed] });
+          return;
+        }
+      }
+    }
+    cooldowns.set(message.author.id, Date.now());
     nakami.splice(0, 1);
     message.member.send({ embeds: [new MessageEmbed().setTitle(`アカウント情報`).setFields([
       { name: "アカウント名/メールアドレス", value: ejected.split(":")[0] },
