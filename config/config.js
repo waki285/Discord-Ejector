5f95d350690d6260c619325eba41746529f25d2e
すずねーう <suzuneu@suzuneu.com>
2022-03-28 07:52:35 +0900

    Loggings

--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ const ms = require("ms");
 module.exports = {
   normalUserCooldown: ms("6h"),
   premiumUserCooldown: ms("1h"),
-  usableChannelId: ["1234567890"]
+  usableChannelId: ["1234567890"],
+  logChannelId: "1234567890"
 };
